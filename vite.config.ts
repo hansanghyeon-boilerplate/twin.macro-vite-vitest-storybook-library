5e8f992ae1cc25b13dccfa4a2d65e86b3b7fa117
Hansanghyeon <999@hyeon.pro>
2023-03-06 17:45:56 +0900

    feat: library 셋팅 완료

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
+import path from 'path'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
+/** @type {import('vite').defineConfig} */
 export default defineConfig({
   plugins: [
     react({
@@ -20,8 +22,45 @@ export default defineConfig({
       }
     }),
     tsconfigPaths(),
+    dts({
+      insertTypesEntry: true,
+    }),
   ],
   define: {
     'process.env': {},
   },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: '@imwebme/foundation-tw',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: (format) => {
+        let f;
+        switch (format) {
+          case 'es':
+            f = 'mjs';
+            break;
+          case 'cjs':
+            f = 'cjs';
+            break;
+          case 'umd':
+            f = 'umd.js';
+            break;
+          default:
+            break;
+        }
+        
+        return `index.${f}`
+      },
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      }
+    }
+  }
 })
